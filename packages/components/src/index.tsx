9efa4b2da45095edebdafc89b366012125656132
Hilary liu <2788370451@qq.com>
2025-06-19 18:23:09 +0800

    feat: 导出节流按钮

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -3,8 +3,9 @@ export * from "./dynamicIcon/DynamicIcon";
 export * from "./modal/Modal";
 export * from "./iconPicker/IconPicker";
 export * from "./table/Table";
-export * from "antd";
+export * from "./throttleButton/ThrottleButton";
 
 // antd语言包
+export * from "antd";
 export { default as zhCN } from "antd/locale/zh_CN";
 export { default as enUS } from "antd/locale/en_US";
