e22bb71b7653995dcdfe4d0111bd1b8d9b285db7
Hilary liu <2788370451@qq.com>
2025-06-19 13:40:20 +0800

    feat: 导出antd组件和语言包

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -3,3 +3,8 @@ export * from "./dynamicIcon/DynamicIcon";
 export * from "./modal/Modal";
 export * from "./iconPicker/IconPicker";
 export * from "./table/Table";
+export * from "antd";
+
+// antd语言包
+export { default as zhCN } from "antd/locale/zh_CN";
+export { default as enUS } from "antd/locale/en_US";
